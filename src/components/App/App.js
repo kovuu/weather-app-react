43f727b4f0cb418f0528dedfd88fcac2773e7815
kovuu <70741965+kovuu@users.noreply.github.com>
2020-10-05 12:27:56 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import './App.css';
 import Layout from "../Layout/Layout";
 
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => (
     <BrowserRouter>
         <Switch>
             <Route path={'/:city/:mode'} component={Layout}/>
+            <Redirect from="/" to="/taganrog/today" />
         </Switch>
     </BrowserRouter>
 )
